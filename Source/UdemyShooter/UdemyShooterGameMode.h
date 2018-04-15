34a9208f8267e1182d07b9dc55a3ccdd64d46a56
DESKTOP-PDIU87N\Theodor <theodor.lukacs195@gmail.com>
2018-04-15 17:20:24 +0300

    Initial commit

---
+++ Source/UdemyShooter/UdemyShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUdemyShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUdemyShooterGameMode();
+};
+
+
+
