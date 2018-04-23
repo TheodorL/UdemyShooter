6edbd6406046cdd2f49a12ad1f6562fec8557f51
DESKTOP-PDIU87N\Theodor <theodor.lukacs195@gmail.com>
2018-04-23 21:58:24 +0300

    Commit 3 : AI sensing + Animation assets.

--- Source/UdemyShooter/Private/GetNextWaypoint.cpp
+++ Source/UdemyShooter/Private/GetNextWaypoint.cpp
@@ -3,24 +3,35 @@
 #include "GetNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingThirdPersonChar.h"
+#include "PatrolComponent.h"
 
 
 EBTNodeResult::Type UGetNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	APawn * ControllerPawn = OwnerComp.GetAIOwner()->GetPawn();
-	APatrollingThirdPersonChar * PatrollingChar = Cast<APatrollingThirdPersonChar>(ControllerPawn);
-	TArray<AActor*> PatrolPoints = PatrollingChar->PatrolPointsCPP;
+	APawn * ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolComponent = ControlledPawn->FindComponentByClass<UPatrolComponent>();
+
+	if (!ensure(PatrolComponent)) { return EBTNodeResult::Failed;  }
+
+	TArray<AActor*> PatrolPoints = PatrolComponent->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s has no patrol points"), *ControlledPawn->GetName());
+		return EBTNodeResult::Failed;
+	}
+
 	UBlackboardComponent * BlackboardComp = OwnerComp.GetBlackboardComponent();
 
 	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
 	int32 NextIndex = (Index + 1) % PatrolPoints.Num();
+
 	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
-	UE_LOG(LogTemp, Warning, TEXT("%i"), Index);
-
 	return EBTNodeResult::Succeeded;
+
 }
 
 
